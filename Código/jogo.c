5628e99c335f46397dd07da893a7c9631c11ae4c
Ivis Muzi <118779139+ivismma@users.noreply.github.com>
2024-01-16 01:55:29 -0300

    Update jogo.c

--- Código/jogo.c
+++ Código/jogo.c
@@ -1,17 +1,17 @@
 #include <stdbool.h>
+#include <ncurses/ncurses.h>
+#include <time.h>
 
 void inicializarMinhoca(Minhoca *minhoca){
-	// Inicializa a minhoca com uma cabeÁa e uma parte.
+	// Inicializa a minhoca com uma cabe√ßa e uma parte.
 	minhoca->x = WIDTH/2;
     minhoca->y = HEIGHT/2;
 	minhoca[1].x = minhoca->x; 
 	minhoca[1].y = minhoca->y-1;
 }
-#include <stdbool.h>
-#include <ncurses/ncurses.h>
 
 void inicializarMinhoca(Minhoca *minhoca){
-	// Inicializa a minhoca com uma cabeÁa e uma parte.
+	// Inicializa a minhoca com uma cabe√ßa e uma parte.
 	minhoca->x = WIDTH/2;
     minhoca->y = HEIGHT/2;
 	minhoca[1].x = minhoca->x; 
@@ -33,42 +33,42 @@ void desenharMinhoca(Minhoca *minhoca, int tam){
 		mvprintw((minhoca+i)->y, (minhoca+i)->x, "O");
 }
 
-// x, y correspondem ‡ posiÁ„o atual que se encontrava a cabeÁa da minhoca.
+// x, y correspondem √† posi√ß√£o atual que se encontrava a cabe√ßa da minhoca.
 void atualizarMinhoca(Minhoca *minhoca, int *tam){
 	for (int i = *tam - 1; i > 0; --i)
         minhoca[i] = minhoca[i-1];
-	// Atualizando a cabeÁa da minhoca
-    // Verificar se a minhoca comeu a maÁ„, se sim, aumentar seu tamanho.
+	// Atualizando a cabe√ßa da minhoca
+    // Verificar se a minhoca comeu a ma√ß√£, se sim, aumentar seu tamanho.
     refresh();
 }
 bool checaColisao(Minhoca *minhoca, int x, int y){
-	// Colis„o com parede vertical
+	// Colis√£o com parede vertical
 	if(y == 0 || y >= HEIGHT+1) return true;
-	// Colis„o com parede horizontal
+	// Colis√£o com parede horizontal
 	else if(x == 0 || x >= WIDTH+1) return true;
 
-	else return false; // N„o haver· colis„o.
+	else return false; // N√£o haver√° colis√£o.
 }
 
 bool checaMorte(Minhoca *minhoca, int tam){
 	for(int i = 1; i < tam;++i){
 		if(minhoca->x == minhoca[i].x && minhoca->y == minhoca[i].y)
-			return true; // Colis„o
+			return true; // Colis√£o
 	}
-	// A cabeÁa da minhoca n„o colidiu com si mesma.
+	// A cabe√ßa da minhoca n√£o colidiu com si mesma.
 	return false;
 }
 
 Consumivel gerarConsumivel(Minhoca *minhoca,  int tam){
 	srand(time(NULL));
 	bool disponivel; 
-	do{ // Tenta criar uma maÁ„ em um lugar disponÌvel.
+	do{ // Tenta criar uma ma√ß√£ em um lugar dispon√≠vel.
 		disponivel = true;
 		pos.x = rand()%WIDTH + 1;
 		pos.y = rand()%HEIGHT + 1;
 		for(int i = 0; i < tam; ++i){
 			if(pos.x == minhoca[i].x && pos.y == minhoca[i].y)
-				disponivel = false; // Coordenada gerada est· ocupada pela minhoca.
+				disponivel = false; // Coordenada gerada est√° ocupada pela minhoca.
 		}
 	} while(!disponivel);
 	mvprintw(pos.y, pos.x, "-");
@@ -93,13 +93,13 @@ void crescerMinhoca(Minhoca *minhoca, int *tam){
 
 void mostrarInfo(){
 	mvprintw(0,34,"Em desenvolvimento...");
-	mvprintw(2,34,"ImplementaÁıes que ainda faltam:");
+	mvprintw(2,34,"Implementa√ß√µes que ainda faltam:");
 	mvprintw(4,34,"impedir minhoca de se movimentar a si mesmo (morte),");
-	mvprintw(5,34,"reinÌcio da rodada apÛs morte, sistema de pontuaÁ„o, mensagem de fim de jogo.");
+	mvprintw(5,34,"rein√≠cio da rodada ap√≥s morte, sistema de pontua√ß√£o, mensagem de fim de jogo.");
 	mvprintw(6,34,"adicionar sons de efeito (SFX) e outros.");
 	
-	mvprintw(8,34,"ImplementaÁıes j· criadas:");
-	mvprintw(10,34,"GeraÁ„o do mapa, GeraÁ„o da maÁ„, definiÁ„o de paredes, colis„o, ");
-	mvprintw(11,34,"transferir minhoca para lado oposto da colis„o, proteÁ„o p/ entrada inv·lida");
-	mvprintw(12,34,"e sistema que controla a trajetÛria da minhoca (Vetor alocado dinamicamente).");
+	mvprintw(8,34,"Implementa√ß√µes j√° criadas:");
+	mvprintw(10,34,"Gera√ß√£o do mapa, Gera√ß√£o da ma√ß√£, defini√ß√£o de paredes, colis√£o, ");
+	mvprintw(11,34,"transferir minhoca para lado oposto da colis√£o, prote√ß√£o p/ entrada inv√°lida");
+	mvprintw(12,34,"e sistema que controla a trajet√≥ria da minhoca (Vetor alocado dinamicamente).");
 }
