f568bc883788b243dcf0c2fb711adc929796b342
Ivis Muzi <118779139+ivismma@users.noreply.github.com>
2024-01-21 15:59:53 -0300

    Update main.c

--- Código/main.c
+++ Código/main.c
@@ -7,29 +7,28 @@
 
 #include "header.h"
 
-Consumivel pos; // Vari·vel global - PosiÁ„o atual da maÁ„
+Consumivel pos; // Vari√°vel global - Posi√ß√£o atual da ma√ß√£
 
 int main(){
 	setlocale(LC_ALL, "pt_BR.UTF-8");
-	// FunÁıes biblioteca ncurses
+	// Fun√ß√µes biblioteca ncurses
     initscr();
     clear();
     noecho();
     curs_set(0); 
     keypad(stdscr, TRUE); // leitura teclas especiais
-    int tamanhoM = 3; // Tamanho m·ximo atingido.
     
 	int direcao = KEY_DOWN;
 	int anterior = KEY_DOWN;
 	int tamanho = 3; // Tamanho da minhoca, inicia como 3.
+	int tamanhoM = 3; // Tamanho m√°ximo atingido.
 	int cont = 0; // Movimentos
-	bool minhocaViva = true;
 	bool jogar = true;
-	// AlocaÁ„o de memÛria da minhoca.
+	// Aloca√ß√£o de mem√≥ria da minhoca.
 	Minhoca *minhoca = (Minhoca*) calloc(WIDTH*HEIGHT, sizeof(Minhoca));
 	if(minhoca == NULL){
 		endwin();
-		fprintf(stderr, "Falha na alocaÁ„o de memÛria.\n");
+		fprintf(stderr, "Falha na aloca√ß√£o de mem√≥ria.\n");
 		return -1;
 	}
 	
@@ -48,29 +47,29 @@ int main(){
 		
 		// Captar movimento:
 		int tecla;
-		do tecla = getch(); // Enquanto movimento for inv·lido.
+		do tecla = getch(); // Enquanto movimento for inv√°lido.
 		while(!checaMovimento(anterior, tecla)); 
 		
 		anterior = tecla;
 		++cont; // Movs.
 		usleep(10000);  // microsegundos
 		
-		// PosiÁ„o do rabo (tail) da minhoca:
+		// Posi√ß√£o do rabo (tail) da minhoca:
 		int tailX = minhoca[tamanho-1].x;
 		int tailY = minhoca[tamanho-1].y;
 		
-		// Computar tecla e verificar se prÛximo movimento ter· colis„o com parede.
+		// Computar tecla e verificar se pr√≥ximo movimento ter√° colis√£o com parede.
 		bool colidiu = false;
 		colidiu = preComputarMovimento(tecla, minhoca, tamanho, &direcao);
 		if(!colidiu) movimentoNormal(direcao, minhoca, tamanho);
 		
-		// Checar se a minhoca morreu apÛs o movimento efetuado.
+		// Checar se a minhoca morreu ap√≥s o movimento efetuado.
 	    if(checaMorte(minhoca, tamanho)){
 			jogar = selecionar();
 			if(jogar) reiniciaJogo(minhoca, &tamanho, &cont);
 		}
 		
-		// Checar se a minhoca comeu a maÁ„ com o movimento efetuado.
+		// Checar se a minhoca comeu a ma√ß√£ com o movimento efetuado.
     	else if(checaSeComeu(minhoca, pos)){
         	crescerMinhoca(minhoca, &tamanho, tailX, tailY);
         	pos = gerarConsumivel(minhoca, tamanho);
